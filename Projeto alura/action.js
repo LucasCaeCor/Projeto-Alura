6e0d87460071f1e397ef3323ebcfb64665543ed8
Lucas Caetano Cordeiro <146130536+LucasCaeCor@users.noreply.github.com>
2024-09-05 08:32:15 -0300

    Add files via upload

---
+++ Projeto alura/action.js
@@ -0,0 +1,18 @@
+let dados = [ // utilizando o Let vc define  
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "blabbla bla blabbla bla blabbla bla",
+        link : "https://www.google.com",
+    },
+    {
+        titulo : "Rambo",
+        descricao : "blabbla bla blabbla bla blabbla bla",
+        link : "https://www.google.com",
+    },
+    {
+        titulo : "Rayssa Leal",
+        descricao : "blabbla bla blabbla bla blabbla bla",
+        link : "https://www.google.com",
+    }
+]; //com colchetes vc abre uma lista, com chaves somente uma informação ou logica[
+
